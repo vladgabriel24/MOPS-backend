161e37151d1c95d1f4142fa1022718f7f3e71cd4
Athanasia Popa <atha.popa@gmail.com>
2025-01-13 01:58:08 +0200

    Add cart manipulation APIs

--- app/models.py
+++ app/models.py
@@ -8,123 +8,120 @@ class User(db.Model):
     invoiceAddress = db.Column(db.String(100), nullable=True)
 
     def __repr__(self):
-        return f"User('{self.firebaseID}', '{self.shippingAddress}', '{self.invoiceAddress}', {self.idCartItem})"
-
+        return f"User('{self.firebaseID}', '{self.shippingAddress}', '{self.invoiceAddress}')"
 
 class Disc(db.Model):
-    __tablename__ = 'Disc'  
+    __tablename__ = 'Disc'
 
-    idDisc = db.Column(db.Integer, primary_key=True, autoincrement=True)  
-    label = db.Column(db.String(100), nullable=True) 
-    title = db.Column(db.String(100), nullable=True)  
-    format = db.Column(db.String(100), nullable=True)  
-    price = db.Column(db.Integer, nullable=True)  
+    idDisc = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(100))
+    title = db.Column(db.String(100))
+    format = db.Column(db.String(100))
+    price = db.Column(db.Integer)
 
     def __repr__(self):
         return f"Disc('{self.idDisc}', '{self.label}', '{self.title}', '{self.format}', {self.price})"
-    
+
+# Artist Model
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
-    idArtist = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nameArtist = db.Column(db.String(100), nullable=True)
+    idArtist = db.Column(db.Integer, primary_key=True)
+    nameArtist = db.Column(db.String(100))
 
     def __repr__(self):
         return f"Artist('{self.idArtist}', '{self.nameArtist}')"
 
-
+# Genre Model
 class Genre(db.Model):
     __tablename__ = 'Genre'
 
-    idGenre = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nameGenre = db.Column(db.String(100), nullable=True)
+    idGenre = db.Column(db.Integer, primary_key=True)
+    nameGenre = db.Column(db.String(100))
 
     def __repr__(self):
         return f"Genre('{self.idGenre}', '{self.nameGenre}')"
 
-
-class CartItem(db.Model):
-    __tablename__ = 'CartItem'
-
-    idCartItem = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    discItem = db.Column(db.String(100), nullable=True)
-    quantity = db.Column(db.Integer, nullable=True)
-    firebaseID = db.Column(db.Integer, db.ForeignKey('User.firebaseID'), nullable=True)
-
-    # Relationship with User
-    cart_item = db.relationship('User', backref='cart_item', lazy=True)
-
-
-    def __repr__(self):
-        return f"CartItem('{self.idCartItem}', '{self.discItem}', {self.quantity})"
-
+# Song Model
 class Song(db.Model):
     __tablename__ = 'Song'
 
-    idSong = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    songName = db.Column(db.String(100), nullable=True)
-    releaseDate = db.Column(db.Date, nullable=True)
+    idSong = db.Column(db.Integer, primary_key=True)
+    songName = db.Column(db.String(100))
+    releaseDate = db.Column(db.Date)
 
     def __repr__(self):
         return f"Song('{self.idSong}', '{self.songName}', '{self.releaseDate}')"
 
+# User Model
+
+# CartItem Model
+class CartItem(db.Model):
+    __tablename__ = 'CartItem'
+
+    idCartItem = db.Column(db.Integer, primary_key=True)
+    firebaseID = db.Column(db.Integer, db.ForeignKey('User.firebaseID'))
+    discItem = db.Column(db.String(100))
+    quantity = db.Column(db.Integer)
+
+    user = db.relationship('User', backref=db.backref('cart_items', lazy=True))
+
+    def __repr__(self):
+        return f"CartItem('{self.idCartItem}', '{self.firebaseID}', '{self.discItem}', {self.quantity})"
+
+# SpotifyLikedSong Model
 class SpotifyLikedSong(db.Model):
     __tablename__ = 'SpotifyLikedSong'
 
-    idSpotifyLikedSong = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    firebaseID = db.Column(db.Integer, db.ForeignKey('User.firebaseID'), nullable=False)
-    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'), nullable=False)
+    idSpotifyLikedSong = db.Column(db.Integer, primary_key=True)
+    firebaseID = db.Column(db.Integer, db.ForeignKey('User.firebaseID'))
+    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'))
 
-    # Relationship with User and Song
-    user = db.relationship('User', backref='spotify_liked_songs', lazy=True)
-    song = db.relationship('Song', backref='spotify_liked_songs', lazy=True)
+    user = db.relationship('User', backref=db.backref('spotify_liked_songs', lazy=True))
+    song = db.relationship('Song', backref=db.backref('spotify_liked_songs', lazy=True))
 
     def __repr__(self):
-        return f"SpotifyLikedSong('{self.idSpotifyLikedSong}', {self.firebaseID}, {self.idSong})"
+        return f"SpotifyLikedSong('{self.idSpotifyLikedSong}', '{self.firebaseID}', '{self.idSong}')"
 
+# SongGenre Model
 class SongGenre(db.Model):
     __tablename__ = 'SongGenre'
 
-    idSongGenre = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    idGenre = db.Column(db.Integer, db.ForeignKey('Genre.idGenre'), nullable=False)
-    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'), nullable=False)
+    idSongGenre = db.Column(db.Integer, primary_key=True)
+    idGenre = db.Column(db.Integer, db.ForeignKey('Genre.idGenre'))
+    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'))
 
-    # Relationships with Genre and Song
-    genre = db.relationship('Genre', backref='song_genres', lazy=True)
-    song = db.relationship('Song', backref='song_genres', lazy=True)
+    genre = db.relationship('Genre', backref=db.backref('song_genres', lazy=True))
+    song = db.relationship('Song', backref=db.backref('song_genres', lazy=True))
 
     def __repr__(self):
-        return f"SongGenre('{self.idSongGenre}', {self.idGenre}, {self.idSong})"
-    
+        return f"SongGenre('{self.idSongGenre}', '{self.idGenre}', '{self.idSong}')"
 
+# DiscSong Model
 class DiscSong(db.Model):
     __tablename__ = 'DiscSong'
 
-    idDiscSong = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    idDisc = db.Column(db.Integer, db.ForeignKey('Disc.idDisc'), nullable=False)
-    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'), nullable=False)
+    idDiscSong = db.Column(db.Integer, primary_key=True)
+    idDisc = db.Column(db.Integer, db.ForeignKey('Disc.idDisc'))
+    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'))
 
-    # Relationships with Disc and Song
-    disc = db.relationship('Disc', backref='disc_songs', lazy=True)
-    song = db.relationship('Song', backref='disc_songs', lazy=True)
+    disc = db.relationship('Disc', backref=db.backref('disc_songs', lazy=True))
+    song = db.relationship('Song', backref=db.backref('disc_songs', lazy=True))
 
     def __repr__(self):
-        return f"DiscSong('{self.idDiscSong}', {self.idDisc}, {self.idSong})"
+        return f"DiscSong('{self.idDiscSong}', '{self.idDisc}', '{self.idSong}')"
 
+# SongArtistFeature Model
 class SongArtistFeature(db.Model):
     __tablename__ = 'SongArtistFeature'
 
-    idSongArtistFeature = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'), nullable=False)
-    idArtist = db.Column(db.Integer, db.ForeignKey('Artist.idArtist'), nullable=False)
-    importance = db.Column(db.Integer, nullable=False)
+    idSongArtistFeature = db.Column(db.Integer, primary_key=True)
+    idSong = db.Column(db.Integer, db.ForeignKey('Song.idSong'))
+    idArtist = db.Column(db.Integer, db.ForeignKey('Artist.idArtist'))
+    importance = db.Column(db.Integer)
 
-    # Relationships with Song and Artist
-    song = db.relationship('Song', backref='song_artist_features', lazy=True)
-    artist = db.relationship('Artist', backref='song_artist_features', lazy=True)
+    song = db.relationship('Song', backref=db.backref('song_artist_features', lazy=True))
+    artist = db.relationship('Artist', backref=db.backref('song_artist_features', lazy=True))
 
     def __repr__(self):
-        return f"SongArtistFeature('{self.idSongArtistFeature}', {self.idSong}, {self.idArtist}, {self.importance})"
-
-
-
+        return f"SongArtistFeature('{self.idSongArtistFeature}', '{self.idSong}', '{self.idArtist}', {self.importance})"
